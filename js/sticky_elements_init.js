342beeda80a9348f7a27c437c5bb5bb99527ae73
Chris Kinch <christopher_kinch@dennis.co.uk>
2018-08-12 22:52:28 +0100

    DD-349 Swtich to Webpack build pipline for init JS

---
+++ js/sticky_elements_init.js
@@ -0,0 +1,27 @@
+import StickyElements from '../../../../libraries/sticky_elements_js';
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let { sticky_elements } = Drupal.settings;
+
+    // StickyElements entry point
+    StickyElements.init(sticky_elements);
+
+    // We rejig the slot on DFP render
+    googletag.pubads().addEventListener('slotRenderEnded', function(event) {
+        let slotSelector = '#' + event.slot.getSlotElementId();
+        let slotElement = document.querySelector(slotSelector);
+
+        sticky_elements.forEach(element => {
+            let slotChild = element.target.querySelector(slotSelector);
+            
+            if(element.target === slotElement || slotChild){
+                if(element.type !== 'timeout') {
+                this.setHeight(element.parent, element.value);
+                }else{
+                this.setHeight(element.target);
+                }
+                this.setAllEnds(element);
+            }
+        });
+    }.bind(StickyElements));
+});
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+    entry: require.resolve("./js/sticky_elements_init.js"),
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "sticky_elements_init.min.js"
+    },
+    watchOptions: {
+        poll: 200 // Check for changes every second
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+};
