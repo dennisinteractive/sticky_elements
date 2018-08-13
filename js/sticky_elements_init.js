873e491756a0a8e020a51e7c099971744fce07ac
Chris Kinch <kinchie@gmail.com>
2018-08-13 11:48:28 +0100

    DD-349 Init code to trigger sticky fucntionality

--- js/sticky_elements_init.js
+++ js/sticky_elements_init.js
@@ -1,27 +1,40 @@
-import StickyElements from '../../../../libraries/sticky_elements_js';
+(function () {
+  Drupal.behaviors.stickyElementsBehavior = {
+    attach: function (context, settings) {
+      let { sticky_elements } = settings;
+      
+      var initSticky = function() {
+        // Main init entry
+        StickyElements.init(sticky_elements);
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    let { sticky_elements } = Drupal.settings;
+        // We rejig the slot on in the event that a DFP slot is being targeted
+        googletag.pubads().addEventListener('slotRenderEnded', function(event) {
+          let slotSelector = '#' + event.slot.getSlotElementId();
+          let slotElement = document.querySelector(slotSelector);
 
-    // StickyElements entry point
-    StickyElements.init(sticky_elements);
+          sticky_elements.elements.forEach(element => {
+              let slotChild = element.target.querySelector(slotSelector);
+              
+              if(element.target === slotElement || slotChild){
+                  if(element.type !== 'timeout') {
+                  this.setHeight(element.parent, element.value);
+                  }else{
+                  this.setHeight(element.target);
+                  }
+                  this.setAllEnds(element);
+              }
+          });
+        }.bind(StickyElements));
+      }
 
-    // We rejig the slot on DFP render
-    googletag.pubads().addEventListener('slotRenderEnded', function(event) {
-        let slotSelector = '#' + event.slot.getSlotElementId();
-        let slotElement = document.querySelector(slotSelector);
-
-        sticky_elements.elements.forEach(element => {
-            let slotChild = element.target.querySelector(slotSelector);
-            
-            if(element.target === slotElement || slotChild){
-                if(element.type !== 'timeout') {
-                this.setHeight(element.parent, element.value);
-                }else{
-                this.setHeight(element.target);
-                }
-                this.setAllEnds(element);
-            }
+      // If StickyElements is loaded then go for it. If not wait for the load event.
+      if(StickyElements) {
+        initSticky();
+      }else {
+        document.addEventListener("sticky-element:load", function(event){
+          initSticky();
         });
-    }.bind(StickyElements));
-});
+      }
+    }
+  };
+})();
