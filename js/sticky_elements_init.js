5b9292102924ca51ed13e0e1d6e031f51f8e7f57
David Grayston <david_grayston@dennis.co.uk>
2018-09-26 22:53:39 +0100

    Issue #3002582 use googletag.cmd.push to prevent JS error

--- js/sticky_elements_init.js
+++ js/sticky_elements_init.js
@@ -8,23 +8,25 @@
           StickyElements.init(settings.sticky_elements);
 
           // We rejig the slot on in the event that a DFP slot is being targeted
-          googletag.pubads().addEventListener('slotRenderEnded', function(event) {
-            let slotSelector = '#' + event.slot.getSlotElementId();
-            let slotElement = document.querySelector(slotSelector);
+          googletag.cmd.push(function() {
+            googletag.pubads().addEventListener('slotRenderEnded', function (event) {
+              let slotSelector = '#' + event.slot.getSlotElementId();
+              let slotElement = document.querySelector(slotSelector);
 
-            settings.sticky_elements.elements.forEach(element => {
+              settings.sticky_elements.elements.forEach(element = > {
                 let slotChild = element.target.querySelector(slotSelector);
 
-                if(element.target === slotElement || slotChild){
-                  if(element.type !== 'timeout') {
+                if (element.target === slotElement || slotChild) {
+                  if (element.type !== 'timeout') {
                     this.setDimensions(element.parent, element.value);
-                  }else{
+                  } else {
                     this.setDimensions(element.target);
                   }
                   this.setAllEnds(element);
                 }
-            });
-          }.bind(StickyElements));
+              });
+            }.bind(StickyElements));
+          });
         }
 
         // If StickyElements is loaded then go for it. If not wait for the load event.
