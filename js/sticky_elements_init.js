987b9d89db1c4bc2b97fed0767f5773b797332b4
Chris Kinch <christopher_kinch@dennis.co.uk>
2018-09-27 11:46:10 +0100

    Issue #3002582 Fixing JS error with forEach but also converted to ES5

--- js/sticky_elements_init.js
+++ js/sticky_elements_init.js
@@ -10,22 +10,22 @@
           // We rejig the slot on in the event that a DFP slot is being targeted
           googletag.cmd.push(function(){
             googletag.pubads().addEventListener('slotRenderEnded', function (event) {
-              let slotSelector = '#' + event.slot.getSlotElementId();
-              let slotElement = document.querySelector(slotSelector);
+              var slotSelector = '#' + event.slot.getSlotElementId();
+              var slotElement = document.querySelector(slotSelector);
 
-              settings.sticky_elements.elements.forEach(element = > {
-                let slotChild = element.target.querySelector(slotSelector);
+              settings.sticky_elements.elements.forEach(function(element){
+                var slotChild = element.target.querySelector(slotSelector);
 
                 if (element.target === slotElement || slotChild) {
                   if (element.type !== 'timeout') {
-                    this.setDimensions(element.parent, element.value);
+                    StickyElements.setDimensions(element.parent, element.value);
                   } else {
-                    this.setDimensions(element.target);
+                    StickyElements.setDimensions(element.target);
                   }
-                  this.setAllEnds(element);
+                  StickyElements.setAllEnds(element);
                 }
               });
-            }.bind(StickyElements));
+            });
           });
         }
 
