2e26565e0bd874eb1f0e81cd185c841260362b99
Chris Kinch <christopher_kinch@dennis.co.uk>
2018-10-08 16:51:58 +0100

    3005156 Updating to new version of the library

--- js/sticky_elements_init.js
+++ js/sticky_elements_init.js
@@ -2,41 +2,33 @@
   if (Drupal) {
     Drupal.behaviors.stickyElementsBehavior = {
       attach: function (context, settings) {
-
-        function initSticky() {
-          // Main init entry
-          StickyElements.init(settings.sticky_elements);
-
-          // We rejig the slot on in the event that a DFP slot is being targeted
-          googletag.cmd.push(function(){
-            googletag.pubads().addEventListener('slotRenderEnded', function (event) {
-              var slotSelector = '#' + event.slot.getSlotElementId();
-              var slotElement = document.querySelector(slotSelector);
-
-              settings.sticky_elements.elements.forEach(function(element){
-                var slotChild = element.target.querySelector(slotSelector);
-
-                if (element.target === slotElement || slotChild) {
-                  if (element.type !== 'timeout') {
-                    StickyElements.setDimensions(element.parent, element.value);
-                  } else {
-                    StickyElements.setDimensions(element.target);
-                  }
-                  StickyElements.setAllEnds(element);
-                }
-              });
-            });
-          });
-        }
-
         // If StickyElements is loaded then go for it. If not wait for the load event.
         if(StickyElements) {
-          initSticky();
+          StickyElements.init(settings.sticky_elements);
         }else {
           document.addEventListener("sticky-element:load", function(event){
-            initSticky();
+            StickyElements.init(settings.sticky_elements);
           }.bind(window));
         }
+        
+        googletag.cmd.push(function(){
+          googletag.pubads().addEventListener('slotRenderEnded', function (event) {
+            var slotSelector = '#' + event.slot.getSlotElementId();
+            var slotElement = document.querySelector(slotSelector);
+
+            settings.sticky_elements.elements.forEach(function(element){
+              var slotChild = element.target.querySelector(slotSelector);
+
+              if (element.target === slotElement || slotChild) {
+                if(element.type !== "timeout") {
+                  StickyElements.resetParent(element);
+                }else{
+                  StickyElements.setTimeoutTriggers();
+                }
+              }
+            });
+          });
+        });
       }
     };
   }
